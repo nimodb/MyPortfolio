b113e46ba87eb304cb872667e557091f33c9f3c5
Nima Saadati <nimodbdev@gmail.com>
2024-06-05 14:04:41 +0400

    Add avatar field to Profile model

--- MyPortfolio/MyPortfolio/settings.py
+++ MyPortfolio/MyPortfolio/settings.py
@@ -29,9 +29,9 @@ environ.Env.read_env()
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env("DEBUG")
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
